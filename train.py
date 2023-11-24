9c2c7a997ab1d4abe253d7ba8ac03e359a0d0191
Hou Chengkai <96556513+JackHck@users.noreply.github.com>
2023-11-24 20:52:56 +0800

    Update train.py

--- train.py
+++ train.py
@@ -17,13 +17,11 @@ from merger.merger_net import Net
 from merger.composed_chamfer import loss_all
 
 
-arg_parser = argparse.ArgumentParser(description="Training Key_Grid")
+arg_parser = argparse.ArgumentParser(description="Training Key_Grid for the PointNet++ on the ShapeNet dataset.")
 arg_parser.add_argument('-m', '--checkpoint-path', '--model-path', type=str, default='./shapenet/model/chair_10.pt',
                         help='Model checkpoint file path for saving.')
 arg_parser.add_argument('-k', '--n-keypoint', type=int, default=10,
                         help='Requested number of keypoints to detect.')
-arg_parser.add_argument('-d', '--device', type=str, default='cpu',
-                        help='Pytorch device for training.')
 arg_parser.add_argument('-b', '--batch', type=int, default=8,
                         help='Batch size.')
 arg_parser.add_argument('-e', '--epochs', type=int, default=100,
@@ -43,7 +41,6 @@ def L2(embed):
 def feed(net, optimizer, x_set, train, shuffle, batch, epoch, ns):
     running_init_points = 0.0
     running_chamfer = 0.0
-    running_keypoint = 0.0
     net.train(train)
     if shuffle:
         x_set = list(x_set)
@@ -67,7 +64,7 @@ def feed(net, optimizer, x_set, train, shuffle, batch, epoch, ns):
                 optimizer.step()
     
       
-            print('[%s%d, %4d] init_point: %.4f chamfer: %.4f keypoint: %.4f' %
+            print('[%s%d, %4d] init_point: %.4f chamfer: %.4f
                   ('VT'[train], epoch, i, running_init_points / (i + 1), running_chamfer / (i + 1)))
 
             
@@ -75,8 +72,8 @@ if __name__ == '__main__':
     ns = arg_parser.parse_args()
     batch = ns.batch
     
-    x = np.loadtxt("./shapenet/dataset/traindata14.txt")
-    x = x.reshape(int(x.shape[0]/2048), 2048, 3)
+    x = np.loadtxt("./shapenet/dataset/traindata14.txt")# 14 is the chair dataset.
+    x = x.reshape(int(x.shape[0]/ns.max_points), ns.max_points, 3)
     net = Net(ns.max_points, ns.n_keypoint).cuda()
     optimizer = optim.Adadelta(net.parameters(),lr=0.1,eps=1e-2)
     
