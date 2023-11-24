735f230ddffc9625a21b3c9faa509c5be47c8798
Hou Chengkai <96556513+JackHck@users.noreply.github.com>
2023-11-24 21:24:37 +0800

    Update vision.py

--- vision.py
+++ vision.py
@@ -6,11 +6,11 @@ import numpy as np
 
 
 
-pointcloud = np.loadtxt(r"./testdata") # chairdataset.txt
+pointcloud = np.loadtxt(r"../shapenet/dataset/testdata14.txt") # chairdataset.txt
 pointcloud = pointcloud.reshape(int(pointcloud.shape[0]/2048), 2048, 3)
              
-keypoint = np.loadtxt(r"") #   keypoint.txt 
+keypoint = np.loadtxt(r"./shapenet/keypoint/chair_keypoint.txt") #   keypoint.txt 
 keypoint = keypoint.reshape(int(keypoint.shape[0]/10), 10, 3)
 
 for i in range(keypoint.shape[0]):
-   visualizer.save_kp_and_pc_in_pcd(pointcloud[i],keypoint[i], '{}_visualizations'.format(r''), save=True,name="{}".format(i))
+   visualizer.save_kp_and_pc_in_pcd(pointcloud[i],keypoint[i], '{}_visualizations'.format(r'./shapenet/visual/chair.txt'), save=True,name="{}".format(i))
