7f4f96b6786e54b75bf8256b7f3c91c5714688bf
Hou Chengkai <96556513+JackHck@users.noreply.github.com>
2024-10-11 16:12:11 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,9 +18,9 @@ from merger.composed_chamfer import loss_all
 
 
 arg_parser = argparse.ArgumentParser(description="Training Key_Grid for the PointNet++ on the ShapeNet dataset.")
-arg_parser.add_argument('-m', '--checkpoint-path', '--model-path', type=str, default='./shapenet/model/chair_10.pt',
+arg_parser.add_argument('-m', '--checkpoint-path', '--model-path', type=str, default='Key_Grid/model/foldpant.pt',
                         help='Model checkpoint file path for saving.')
-arg_parser.add_argument('-k', '--n-keypoint', type=int, default=10,
+arg_parser.add_argument('-k', '--n-keypoint', type=int, default=8,
                         help='Requested number of keypoints to detect.')
 arg_parser.add_argument('-b', '--batch', type=int, default=8,
                         help='Batch size.')
@@ -28,7 +28,7 @@ arg_parser.add_argument('-e', '--epochs', type=int, default=100,
                         help='Number of epochs to train.')
 arg_parser.add_argument('--max-points', type=int, default=2048,
                         help='Indicates maximum points in each input point cloud.')
-arg_parser.add_argument("--keynumber", type=int, help="", default=14)
+arg_parser.add_argument("--keynumber", type=int, help="", default=12)
 arg_parser.add_argument("--chamfer", type=int, help="", default=20)
 arg_parser.add_argument("--lambda_init_points", type=float, help="", default=1.0)
 arg_parser.add_argument("--lambda_chamfer", type=float, help="", default=1.0)
@@ -41,6 +41,7 @@ def L2(embed):
 def feed(net, optimizer, x_set, train, shuffle, batch, epoch, ns):
     running_init_points = 0.0
     running_chamfer = 0.0
+    running_smoothing = 0.0
     net.train(train)
     if shuffle:
         x_set = list(x_set)
@@ -57,6 +58,7 @@ def feed(net, optimizer, x_set, train, shuffle, batch, epoch, ns):
             running_init_points += loss['init_points']
             if epoch> ns.chamfer:
                 running_chamfer += loss['chamfer']
+                running_smoothing += loss['smoothing']
             loss = sum(loss.values())
         
             if train:
@@ -64,21 +66,21 @@ def feed(net, optimizer, x_set, train, shuffle, batch, epoch, ns):
                 optimizer.step()
     
       
-            print('[%s%d, %4d] init_point: %.4f chamfer: %.4f
-                  ('VT'[train], epoch, i, running_init_points / (i + 1), running_chamfer / (i + 1)))
+            print('[%s%d, %4d] init_point: %.4f chamfer: %.4f smooth: %.4f '%
+                  ('VT'[train], epoch, i, running_init_points / (i + 1), running_chamfer / (i + 1), running_smoothing/(i+1)))
 
             
 if __name__ == '__main__':
     ns = arg_parser.parse_args()
     batch = ns.batch
     
-    x = np.loadtxt("./shapenet/dataset/traindata14.txt")# 14 is the chair dataset.
+    x = np.loadtxt("Key_Grid/dataset/fold/pant_seqdata.txt")
     x = x.reshape(int(x.shape[0]/ns.max_points), ns.max_points, 3)
     net = Net(ns.max_points, ns.n_keypoint).cuda()
     optimizer = optim.Adadelta(net.parameters(),lr=0.1,eps=1e-2)
     
     for epoch in range(ns.epochs):
-        feed(net, optimizer, x, True, True, batch, epoch, ns)
+        feed(net, optimizer, x, True, False, batch, epoch, ns)
         torch.save({
             'epoch': epoch,
             'model_state_dict': net.state_dict(),
