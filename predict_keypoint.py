327db3097cc5756f0f58aceff345a9b6b06690bc
Hou Chengkai <96556513+JackHck@users.noreply.github.com>
2024-10-11 16:14:57 +0800

    Update predict_keypoint.py

--- predict_keypoint.py
+++ predict_keypoint.py
@@ -14,11 +14,11 @@ import open3d as o3d
 import os
 
 arg_parser = argparse.ArgumentParser(description="Predictor for Keypoint on KeypointNet dataset.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-arg_parser.add_argument('-m', '--checkpoint-path', '--model-path', type=str, default='./shapenet/model/chair_10.pt',
+arg_parser.add_argument('-m', '--checkpoint-path', '--model-path', type=str, default='Key_Grid/model/foldpant.pt',
                         help='Model checkpoint file path to load.')
 arg_parser.add_argument('-d', '--device', type=str, default='cuda',
                         help='Pytorch device for predicting.')
-arg_parser.add_argument('-k', '--n-keypoint', type=int, default=10,
+arg_parser.add_argument('-k', '--n-keypoint', type=int, default=8,
                         help='Requested number of keypoints to detect.')
 arg_parser.add_argument('--max-points', type=int, default=2048,
                         help='Indicates maximum points in each input point cloud.')
@@ -30,13 +30,13 @@ net.eval()
 
 out_kpcd = []
 
-x = np.loadtxt("./shapenet/dataset/testdata14.txt")
+x = np.loadtxt("Key_Grid/dataset/fold/pant.txt")
 x = x.reshape(int(x.shape[0]/2048), 2048, 3)
 
 with torch.no_grad():
-     key_points, r1= net(torch.FloatTensor(x).to(ns.device),'True')      
+     key_points, r1= net(torch.FloatTensor(x).to(ns.device),'False')      
 for kp in key_points:
     out_kpcd.append(kp)
 for i in range(len(out_kpcd)):
     out_kpcd[i] = out_kpcd[i].cpu().numpy()
-np.savetxt('./shapenet/keypoint/chair_keypoint.txt', np.array(out_kpcd).reshape(-1,np.array(out_kpcd).shape[-1]))    
+np.savetxt('Key_Grid/dataset/fold/pant_keypoint.txt', np.array(out_kpcd).reshape(-1,np.array(out_kpcd).shape[-1]))    
